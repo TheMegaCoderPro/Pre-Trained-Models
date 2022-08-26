4c37127a822ec59d266cb1a550f8b0ad91572625
Pranav Satish Kumar <98470657+TheMegaCoderPro@users.noreply.github.com>
2022-08-26 19:42:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas = create.canvas(300,300);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
